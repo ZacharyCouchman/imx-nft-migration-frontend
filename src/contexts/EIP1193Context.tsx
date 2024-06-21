434f73647e1fec34a712660486be3ebaf3d516a9
Zach Couchman <zachary.couchman@immutable.com>
2024-06-21 15:56:36 -0400

    update provider context

--- src/contexts/EIP1193Context.tsx
+++ src/contexts/EIP1193Context.tsx
@@ -1,10 +1,17 @@
 import { createContext, useEffect, useState } from "react";
-import { Web3Provider } from "@ethersproject/providers";
+import { ExternalProvider } from "@ethersproject/providers";
 import { useWeb3ModalProvider, useWeb3ModalAccount } from "@web3modal/ethers5/react"
 
+export type EIP1193Provider = ExternalProvider & {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  on: (eventType: string, handler: (...args: any) => void) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  removeListener: (eventType: string, handler: (...args: any) => void) => void;
+}
 export interface EIP1193ContextState {
-  provider: Web3Provider | null;
-  setProvider: (provider: Web3Provider | null) => void;
+  provider: EIP1193Provider | null;
+  setProvider: (provider: EIP1193Provider | null) => void;
+  chainId: number | null;
   walletAddress: string;
   setWalletAddress: (address: string) => void;
   isPassportProvider: boolean;
@@ -13,6 +20,7 @@ export interface EIP1193ContextState {
 export const EIP1193Context = createContext<EIP1193ContextState>({
   provider: null,
   setProvider: () => {},
+  chainId: null,
   walletAddress: '',
   setWalletAddress: () => {},
   isPassportProvider: false
@@ -22,8 +30,9 @@ interface EIP1193ContextProvider {
   children: React.ReactNode;
 }
 export const EIP1193ContextProvider = ({children}: EIP1193ContextProvider) => {
-  const [provider, setProvider] = useState<Web3Provider | null>(null);
+  const [provider, setProvider] = useState<EIP1193Provider | null>(null);
   const [walletAddress, setWalletAddress] = useState('');
+  const [chainId, setChainId] = useState<number | null>(null);
   const [isPassport, setIsPassport] = useState(false);
 
   const { walletProvider } = useWeb3ModalProvider()
@@ -36,16 +45,42 @@ export const EIP1193ContextProvider = ({children}: EIP1193ContextProvider) => {
   useEffect(() => {
     if(!walletProvider) {
       setWalletAddress('');
+      setChainId(null);
       setIsPassport(false);
       return;
     }
-    setProvider(new Web3Provider(walletProvider));
+    const getChainId = async () => {
+      setChainId(await walletProvider.request!({method: 'eth_chainId'}));
+    }
+    setProvider(walletProvider as EIP1193Provider);
+    getChainId();
   }, [walletProvider]);
+
+  useEffect(() => {
+    if(!provider) return;
+
+    function setChain(network: string) {
+      console.log(network);
+      setChainId(parseInt(network))
+    }
+    function setAccount(accounts: string[]) {
+      console.log(accounts);
+      setWalletAddress(accounts[0] ?? '');
+    }
+    provider.on('chainChanged', setChain);
+    provider.on('accountsChanged', setAccount);
+
+    return () => {
+      provider.removeListener('chainChanged', setChain);
+      provider.removeListener('accountsChanged', setAccount);
+    }
+  }, [provider])
 
   return (
     <EIP1193Context.Provider value={{
       provider, 
       setProvider,
+      chainId,
       walletAddress,
       setWalletAddress,
       isPassportProvider: isPassport,
